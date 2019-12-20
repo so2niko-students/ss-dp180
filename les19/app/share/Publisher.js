74c187fa349e15e1846759d76ce4a85546ba2587
Nick Sotula <so2niko@gmail.com>
2019-12-20 16:15:49 +0200

    publisher end
    publisher end

--- les19/app/share/Publisher.js
+++ les19/app/share/Publisher.js
@@ -3,22 +3,30 @@ export class Publisher {
         this.subscribes = {};
         this.ctrls = {
             subscribe: this.subscribe.bind(this),
-            notify: this.notify.bind(this)
+            unsubscribe: this.unsubscribe.bind(this),
+            notify: this.notify.bind(this),
+        }
+    }
+
+    checkEvent(event) {
+        if (!this.subscribes.hasOwnProperty(event)) {
+            this.subscribes[event] = [];
         }
     }
 
     subscribe(event, func) {
-        if (!this.subscribes.hasOwnProperty(event)) {
-            this.subscribes[event] = [];
-        }
+        this.checkEvent(event);
         this.subscribes[event].push(func);
     }
 
     notify(event, data) {
-        if (!this.subscribes.hasOwnProperty(event)) {
-            this.subscribes[event] = [];
-        }
+        this.checkEvent(event);
         this.subscribes[event].forEach(sb => sb(data));
+    }
+
+    unsubscribe(event, func) {
+        this.checkEvent(event);
+        this.subscribes[event] = this.subscribes[event].filter(fu => fu != func);
     }
 
 
