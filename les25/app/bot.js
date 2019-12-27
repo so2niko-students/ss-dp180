0ed554f16a9356fd6d80fbb04ccc5106f7f999b6
Nick Sotula <so2niko@gmail.com>
2019-12-27 15:49:26 +0200

    bot
    bot

---
+++ les25/app/bot.js
@@ -0,0 +1,22 @@
+fetch('https://api.telegram.org/bot957348871:AAE-U6v_g4toCvwz-3X-uL6C6odnk_0skkk/getUpdates')
+    .then(d => d.json())
+    .then(data => {
+        console.log(data);
+        getIDs(data);
+    });
+
+function getIDs(data) {
+    let idS = JSON.parse(localStorage.getItem('ids') || '{}');
+    data.result.forEach(msg => {
+        idS[msg.message.chat.id] = msg.message.chat.id;
+    });
+    localStorage.setItem('ids', JSON.stringify(idS));
+}
+
+function sendMsg(txt = '<h3>Hello</h3><p>My name is Bot</p>') {
+    let idS = JSON.parse(localStorage.getItem('ids') || '{}');
+    Object.keys(idS).forEach(key => {
+        console.log(key);
+        fetch(`https://api.telegram.org/bot957348871:AAE-U6v_g4toCvwz-3X-uL6C6odnk_0skkk/sendMessage?chat_id=${key}&text=${encodeURIComponent(txt)}&parse_mode=html`);
+    });
+}
