c8a1f28668a182b1942949d6df9a34ed832ea461
Nick Sotula <so2niko@gmail.com>
2020-01-03 15:37:09 +0200

    upd bot info
    upd bot info

--- les25/app/bot.js
+++ les25/app/bot.js
@@ -17,6 +17,12 @@ function sendMsg(txt = '<h3>Hello</h3><p>My name is Bot</p>') {
     let idS = JSON.parse(localStorage.getItem('ids') || '{}');
     Object.keys(idS).forEach(key => {
         console.log(key);
-        fetch(`https://api.telegram.org/bot957348871:AAE-U6v_g4toCvwz-3X-uL6C6odnk_0skkk/sendMessage?chat_id=${key}&text=${encodeURIComponent(txt)}&parse_mode=html`);
+        fetch(`https://api.telegram.org/bot957348871:AAE-U6v_g4toCvwz-3X-uL6C6odnk_0skkk/sendMessage?chat_id=${key}&text=${encodeURIComponent(txt)}&parse_mode=MarkDown`);
     });
 }
+
+let str = `***Hi, user.***
+
+
+My name is BOT`;
+// sendMsg(str);
